27b1c486e7e6c4203197528ca84088fed0a980e0
Hien Nguyen <nguyenhuuhientv@gmail.com>
2016-05-21 16:43:22 +0700

    new project

---
+++ AnhPhatMVC/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AnhPhatMVC.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int id { get; set; }
+        public String username { get; set; }
+        public String password { get; set; }
+        public DateTime birthday { get; set; }
+        public String email { get; set; }
+        public String role { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+    }
+}
