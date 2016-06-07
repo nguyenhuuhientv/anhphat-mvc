6aad5621363c5f4e56c5a13e756b10f80a88ce4e
Hien Nguyen <nguyenhuuhientv@gmail.com>
2016-06-07 22:18:23 +0700

    update route

--- AnhPhatMVC/App_Start/RouteConfig.cs
+++ AnhPhatMVC/App_Start/RouteConfig.cs
@@ -14,6 +14,36 @@ namespace AnhPhatMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "GioiThieu",
+                "{gioi-thieu}",
+                new { controller = "GioiThieu", action = "Index" }
+            );
+
+            routes.MapRoute(
+                "DichVu",
+                "{dich-vu}",
+                new { controller = "DichVu", action = "Index" }
+            );
+
+            routes.MapRoute(
+                "TinTuc",
+                "{tin-tuc}",
+                new { controller = "TinTuc", action = "Index" }
+            );
+
+            routes.MapRoute(
+                "KhuyenMai",
+                "{khuyen-mai}",
+                new { controller = "SanPham", action = "KhuyenMai" }
+            );
+
+            routes.MapRoute(
+                "LienHe",
+                "{lien-he}",
+                new { controller = "LienHe", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
